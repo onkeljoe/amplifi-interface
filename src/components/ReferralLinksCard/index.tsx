e2f7f1090f4c4701a605105a6df8fa05971fd9a9
dunks411 <54425677+duncan4123@users.noreply.github.com>
2022-07-23 13:57:54 +1000

    quick fixes to bring important stuff above the fold

--- src/components/ReferralLinksCard/index.tsx
+++ src/components/ReferralLinksCard/index.tsx
@@ -18,9 +18,21 @@ export const Break = styled.div`
 const RoundedLink = styled.div`
   background: ${({ theme }) => theme.bg3};
   border-radius: 10px;
-  padding: 10px 30px 10px 30px;
+  padding: 5px!important;
+`;
+const Item = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: .3rem;
+  
 `;
 
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 50% 1fr;
+  grid-template-rows: 40px;
+  grid-gap: 5px;
+`;
 export default function ReferralLinksCard() {
   const [activeProtocol] = useActiveProtocol();
   const referralLink = useReferralLink();
@@ -32,7 +44,7 @@ export default function ReferralLinksCard() {
           referralLink ? (
             <>
               <Card>
-                <RoundedLink>
+                <RoundedLink style={{ padding: '5px' }}>
                   <Copy toCopy={"https://" + referralLink}>
                     <span style={{ paddingLeft: 10 }}>
                       {"  "}
@@ -50,18 +62,21 @@ export default function ReferralLinksCard() {
           )
         ) : (
           <Card>
-            <RoundedLink>
-              <p>
-                In order to generate your unique referral link for rewards you
-                must:
-              </p>
-              <ul>
-                <li>Connect your wallet {account ? `-Done` : `-Incomplete`}</li>
-                <li>
-                  Connect your Twitter{" "}
-                  {verifiedHandleEntry ? `-Done` : `-Incomplete`}
-                </li>
-              </ul>
+            <RoundedLink >
+              <Grid>
+                <Item >
+                  To check airdrop / generate referral links you must:
+                </Item>
+                <Item >
+                <ul style={{ padding: 0, margin: 0 }} >
+                  <li>Connect your wallet {account ? <span style={{ color: 'green' }}>✔ Done</span> : <span style={{ color: 'red' }}>❌ Incomplete</span>}</li>
+                  <li>
+                    Connect your Twitter{" "}
+                    {verifiedHandleEntry ? `-Done` : `❌ Incomplete`}
+                  </li>
+                </ul>
+                </Item>
+              </Grid>
             </RoundedLink>
           </Card>
         )}
