1e0c5db249934b68bbe3b7c2aa954f60a995ecb9
jonathanng1122 <jonathanng1122@gmail.com>
2022-08-04 02:31:18 -0700

    f

--- src/subpages/BoostCalculator.tsx
+++ src/subpages/BoostCalculator.tsx
@@ -117,7 +117,7 @@ function BoostCalculator() {
 
   let projectedPayout;
   if (account && beetsScoreBreakdown && cre8rScore && latestCS && cre8rPrice) {
-    projectedPayout = calcProjectedPayouts(account, beetsScoreBreakdown?.beetsScore, cre8rScore / cre8rPrice, latestCS / cre8rPrice, accountLastPayout && (accountLastPayout as any).lastWeekPayoutInCRE8R || 0, cre8rPrice, 640)
+    projectedPayout = calcProjectedPayouts(account, beetsScoreBreakdown?.beetsScore / beetsScoreBreakdown?.fBeetsPrice, cre8rScore / cre8rPrice, latestCS / cre8rPrice, accountLastPayout && (accountLastPayout as any).lastWeekPayoutInCRE8R || 0, cre8rPrice, 640)
     console.log(projectedPayout)
   }
   const loaded =
