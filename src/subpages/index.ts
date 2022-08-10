3f6c072cc4af2db4dfe23fe57e75c35009542476
jonathanng1122 <jonathanng1122@gmail.com>
2022-08-09 22:55:48 -0700

    dynamically add props to reusable subpages

--- src/subpages/index.ts
+++ src/subpages/index.ts
@@ -1,12 +1,34 @@
 import BoostCalculator from "./BoostCalculator";
+
+interface SubPages {
+  [protocolID: string]: {
+    [campaignID: string]: {
+      [tabUri: string]: {
+        Component: (props?: any) => JSX.Element,
+        props?: {[key: string]: string | number}
+      }
+    }
+  }
+}
+
 /**
  * Overrides for tabs
  * This takes priority over WP content
  * key = the end part of the URI
  * value = component to display instead
  */
-const subpages : {[endUri: string]: () => JSX.Element} = {
-  "boost-calculator": BoostCalculator,
+const subpages : SubPages = {
+  'CRE8R': {
+    'beets-boosted-bribes': {
+      'boost-calculator': {
+        Component: BoostCalculator,
+        props: {
+        }
+      }
+    }
+  }
 }
+
+
 
 export default subpages
