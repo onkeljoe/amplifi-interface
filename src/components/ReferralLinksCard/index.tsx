476b7969f7ee9b9dd15a4de67d1171ac24cca0aa
dunks411 <54425677+duncan4123@users.noreply.github.com>
2022-08-20 21:56:35 +1000

    Buttons and logged out works

--- src/components/ReferralLinksCard/index.tsx
+++ src/components/ReferralLinksCard/index.tsx
@@ -39,21 +39,116 @@ export const Break = styled.div`
   margin: 0;
 `;
 
-const RoundedLink = styled.div<{numOfLinks?: number}>`
-  font-size: 12px;
-  background-image: ${({ theme }) => theme.special};
+const RoundedLink = styled.div<{ numOfLinks?: number }>`
+  font-size: 15px;
+  padding: 15px;
+  background-color: #ffbc7d;
   color: ${({ theme }) => theme.white};
   border-radius: 12px;
-  border: solid #dcd4d4;
+  border: solid #ff3700;
   border-width: 1px;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
-  width: ${({numOfLinks}) => numOfLinks ? (100/numOfLinks).toString() + "%" : '100%'};
+  width: ${({ numOfLinks }) =>
+    numOfLinks ? (99 / numOfLinks).toString() + "%" : "100%"};
   ${({ theme }) => theme.mediaWidth.upToSmall`
     width: 100%;
+    flex-gap: 10px;
+    margin-bottom: 10px;
   `};
+  :hover {
+    text-decoration: none;
+    color: ${({ theme }) => theme.text3};
+    background-color: #ff3700;
+  }
+
+  a:hover {
+    color: #ffbc7d;
+  }
+
 `;
+
+const RoundedLinkLoggedOut = styled.div<{ numOfLinks?: number }>`
+  font-size: 15px;
+  padding: 15px;
+  background-color: #ffbc7d;
+  color: ${({ theme }) => theme.white};
+  border-radius: 12px;
+  border: solid #ff3700;
+  border-width: 1px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: ${({ numOfLinks }) =>
+    numOfLinks ? (99 / numOfLinks).toString() + "%" : "100%"};
+  ${({ theme }) => theme.mediaWidth.upToSmall`
+    width: 100%;
+    flex-gap: 10px;
+    margin-bottom: 10px;
+  `};
+  /* :hover {
+    text-decoration: none;
+    color: ${({ theme }) => theme.text3};
+    background-color: #ff3700;
+  }
+
+  a:hover {
+    color: #ffbc7d;
+  } */
+
+`;
+
+export const ReferralCardLink = styled.a`
+  color: #ffbc7d;
+    background-color: '#ff3700';
+  padding: 8px;
+  outline: none;
+  border: 1px solid transparent;
+  border-radius: 12px;
+  text-decoration: none;
+  font-size: 15px;
+  :hover {
+    cursor: pointer;
+    /* opacity: 0.8; */
+    color: '#ff3700!important';
+    background-color: '#ffbc7d';
+  
+  }
+`;
+
+const RoundedLinkTweetintent = styled.div`
+ font-size: 15px;
+
+  color: #ffbc7d;
+  background-color: #ff3700;
+
+
+border-radius: 12px;
+border: solid #ff3700;
+border-width: 1px;
+display: flex;
+justify-content: space-around;
+align-items: center;
+
+${({ theme }) => theme.mediaWidth.upToSmall`
+  width: 100%;
+`};
+:hover {
+  text-decoration: none;
+color: #ff3700;
+background-color: #ffbc7d;
+}
+
+a:hover {
+  color: #ff3700;
+background-color: #ffbc7d;
+}
+  
+  
+`;
+
+
 
 export default function ReferralLinksCard() {
   const [activeProtocol] = useActiveProtocol();
@@ -74,27 +169,25 @@ export default function ReferralLinksCard() {
       {activeProtocol && verifiedHandleEntry ? (
         referralLink ? (
           <>
-            <div style={{
-              padding: 10,
-              display: 'flex',
-              flexWrap: 'wrap'
-            }}>
-              <RoundedLink style={{
-                padding: 10
+            <div
+              style={{
+                padding: 10,
+                display: "flex",
+                flexWrap: "wrap",
+                justifyContent: "space-around",
+                
               }}
-              numOfLinks={twitterIntentUrl ? 2 : 1}
-              >
+            >
+              <RoundedLink numOfLinks={twitterIntentUrl ? 2 : 1}>
                 <Copy toCopy={"https://" + referralLink}>
-                  <div>
-                    <div style={{ paddingLeft: 10 }}>
+                  <div style={{ padding: '5px' }}>
+                    <div>
                       {"  "}
                       Copy your unique link &amp; start earning
                       {/* {utmLinks[activeProtocol?.id]} */}
                     </div>
                     {activeCampaign && (
-                      <div
-                      style={{ fontSize: 8, color: "lightGrey", padding: 1 }}
-                      >
+                      <div style={{ fontSize: "9px", padding: '2px' }}>
                         {activeCampaign.baseUrl.replace("?", "")}
                       </div>
                     )}
@@ -102,11 +195,19 @@ export default function ReferralLinksCard() {
                 </Copy>
               </RoundedLink>
               {twitterIntentUrl && (
-                <RoundedLink numOfLinks={twitterIntentUrl ? 2 : 1}>
-                  <a href={twitterIntentUrl}>
-                    <Logo src={TwitterIcon} alt="twitter logo" />
-                  </a>
-                </RoundedLink>
+                <RoundedLinkTweetintent
+                  style={{ padding: '20px' }}
+                  // numOfLinks={twitterIntentUrl ? 2 : 1}
+                >
+                  <ReferralCardLink style={{textDecoration: 'none'}} href={twitterIntentUrl}>
+                    <div style={{display: "flex", alignItems: "center"}}>
+                      <div style={{ flex:"0 1 auto"}}>Tweet your unique link</div>
+                      <div style={{flex:"0 1 auto"}}>
+                        <Logo src={TwitterIcon} alt="twitter logo" />
+                      </div>
+                    </div>
+                  </ReferralCardLink>
+                </RoundedLinkTweetintent>
               )}
             </div>
           </>
@@ -114,18 +215,22 @@ export default function ReferralLinksCard() {
           <>{/* <Loader /> */}</>
         )
       ) : (
-        <RoundedLink>
-          <div style={{padding: 10}}>
-            <div style={{paddingBottom: 10}}>
+        <RoundedLinkLoggedOut>
+          <div style={{ padding: 10, color: "#ff3700"  }}>
+            <div style={{ paddingBottom: 10 }}>
               To check airdrop and generate referral links you must:
-            </div> 
+            </div>
             <ul style={{ paddingLeft: 20, margin: 0 }}>
               <li>
                 Connect your wallet{" "}
                 {account ? (
-                  <span style={{ color: "green", backgroundColor: "white" }}>✔ Done</span>
+                  <span style={{ color: "green",  }}>
+                    ✔ Done
+                  </span>
                 ) : (
-                  <span style={{ color: "red", backgroundColor: "white" }}>❌ Incomplete</span>
+                  <span style={{ color: "red",  }}>
+                    ❌ Incomplete
+                  </span>
                 )}
               </li>
               <li>
@@ -134,7 +239,7 @@ export default function ReferralLinksCard() {
               </li>
             </ul>
           </div>
-        </RoundedLink>
+        </RoundedLinkLoggedOut>
       )}
       {/* </AutoColumn> */}
     </>
