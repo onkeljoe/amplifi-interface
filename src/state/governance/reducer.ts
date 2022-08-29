79e339b5b84f1a5b0c41c28281dac53092b9e79f
jono <jonathanng1122@gmail.com>
2022-08-28 22:20:12 -0700

    use demo .env

--- src/state/governance/reducer.ts
+++ src/state/governance/reducer.ts
@@ -484,19 +484,23 @@ export const ARGENT_GOVERNANCE: GovernanceInfo = {
 };
 
 // mapping for routing
-export const SUPPORTED_PROTOCOLS: { [id: string]: GovernanceInfo } = {
+export const SUPPORTED_PROTOCOLS: { [id: string]: GovernanceInfo } = process.env.REACT_APP_USE_DEMO ? {
   CRE8R: CRE8R_GOVERNANCE,
-  // DEVO: DEVO_GOVERNANCE, //demo
+  DEVO: DEVO_GOVERNANCE, //demo
   BilliDrop: BILLI_GOVERNANCE, //demo
-  // ACWI: ACWI_GOVERNANCE, //demo
-  // GMX: GMX_GOVERNANCE, //demo
-  // BET: CHAINBET_GOVERNANCE, //demo
-  // POOL: POOL_TOGETHER_GOVERNANCE, //demo
+  ACWI: ACWI_GOVERNANCE, //demo
+  GMX: GMX_GOVERNANCE, //demo
+  BET: CHAINBET_GOVERNANCE, //demo
+  POOL: POOL_TOGETHER_GOVERNANCE, //demo
   AMPLIFI: AMPLIFI_GOVERNANCE,
-  // PALADIN: PALADIN_GOVERNANCE,
-  // ARGENT: ARGENT_GOVERNANCE,
-  // connect: CONNECT_CONFIG,
-};
+  PALADIN: PALADIN_GOVERNANCE,
+  ARGENT: ARGENT_GOVERNANCE,
+  connect: CONNECT_CONFIG,
+} : {
+  CRE8R: CRE8R_GOVERNANCE,
+  BilliDrop: BILLI_GOVERNANCE, 
+  AMPLIFI: AMPLIFI_GOVERNANCE,
+}
 
 export const FETCHING_INTERVAL = 50;
 
